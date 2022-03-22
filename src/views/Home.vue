39a4a9e4c3f06f1b13b7a077b521841fb1ab58b6
hemiahwu <309595700@qq.com>
2022-03-22 19:09:21 +0800

    14

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,22 @@
   <div class="index">初始化页面</div>
 </template>
 
-<script>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import axios from "axios";
+
+onMounted(async () => {
+  try {
+    const data = await axios.get("/api/users/current", {
+      headers: {
+        Authorization: localStorage.token,
+      },
+    });
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
 </script>
 
 <style>
